107438584370e8641e81e4d196a80c142a76ba1d
mickmed <mick2090@gmail.com>
2020-02-11 11:47:08 -0500

    init

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,15 +1,6 @@
-let algos = require("../groupArrays.js")
+let algos = require("../factorialize.js")
 
-test("groups the arrays into an array of arrays of given length, any remainding go in last array", () => {
-  expect(algos.groupArrays(["a", "b", "c", "d"], 4)).toMatchObject([
-    ["a", "b", "c", "d"]
-  ])
-  expect(algos.groupArrays(["a", "b", "c", "d"], 3)).toMatchObject([
-    ["a", "b", "c"],
-    ["d"]
-  ])
-  expect(algos.groupArrays(["a", "b", "c", "d"], 2)).toMatchObject([
-    ["a", "b"],
-    ["c", "d"]
-  ])
+test("factorialize", () => {
+  expect(algos.factorialize(5)).toBe(120)
+
 })
---
+++ factorialize.js
@@ -0,0 +1,13 @@
+//FACTORIALIZE
+const factorialize = num => {
+  var result = 1
+  for (var i = 1; i <= num; i++) {
+    result *= i
+  }
+  return result
+}
+console.log(factorialize(5))
+
+module.exports = {
+  factorialize
+}
