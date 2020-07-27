fa95b178d589b7811156e9f465e834a4746ab042
zmahfuz061318 <str053mgr@gmail.com>
2020-07-26 21:10:16 -0400

    Completed

--- factorialize.js
+++ factorialize.js
@@ -2,6 +2,10 @@
 
 const factorialize = (num) => {
 
+  if(num == 0){
+    return 1;
+  }
+  return num * factorialize(num-1);
 }
 
 console.log(factorialize(4))
@@ -9,3 +13,4 @@ console.log(factorialize(4))
 module.exports = {
   factorialize
 }
+
